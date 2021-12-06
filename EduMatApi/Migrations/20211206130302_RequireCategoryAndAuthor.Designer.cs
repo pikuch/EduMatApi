69a1f381edd0259434d0e4631bea5cdfead09a49
pikuch <55052790+pikuch@users.noreply.github.com>
2021-12-06 14:05:17 +0100

    Add migration adding author and type requirement

--- EduMatApi/Migrations/20211206130302_RequireCategoryAndAuthor.Designer.cs
+++ EduMatApi/Migrations/20211206130302_RequireCategoryAndAuthor.Designer.cs
@@ -4,6 +4,7 @@ using EduMatApi.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EduMatApi.Migrations
 {
     [DbContext(typeof(EduMatApiDbContext))]
-    partial class EduMatApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211206130302_RequireCategoryAndAuthor")]
+    partial class RequireCategoryAndAuthor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
