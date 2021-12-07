f0facbd744ce41ddd9012021249f6dd430ee9fef
pikuch <55052790+pikuch@users.noreply.github.com>
2021-12-07 14:27:25 +0100

    Add custom exception filter with logger

---
+++ EduMatApi/Filters/EduMatApiExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace EduMatApi.Filters
+{
+    public class EduMatApiExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<EduMatApiExceptionFilter> _logger;
+
+        public EduMatApiExceptionFilter(ILogger<EduMatApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            context.ExceptionHandled = true;
+
+            _logger.LogError($"[{DateTime.Now}] Uncought exception: {context.Exception.Message}");
+
+            HttpResponse response = context.HttpContext.Response;
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            response.ContentType = "application/json";
+            response.WriteAsync("An internal server error occured. Sorry!");
+        }
+    }
+}
