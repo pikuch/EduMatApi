929ad6fe7484347879f8346b80facd5b17b3db00
pikuch <55052790+pikuch@users.noreply.github.com>
2021-12-07 18:40:51 +0100

    Fix service configuration mess

--- EduMatApi/Program.cs
+++ EduMatApi/Program.cs
@@ -1,69 +1,13 @@
-using EduMatApi.Authentication;
-using EduMatApi.DAL;
-using EduMatApi.DAL.Repositories;
-using EduMatApi.Filters;
-using EduMatApi.Models.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
+using EduMatApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureServices(
+    builder.Configuration["Jwt:Key"],
+    builder.Configuration["ConnectionStrings:EduMatApiDbConnection"],
+    builder.Configuration["ConnectionStrings:UserDbConnection"]);
 
-builder.Services.AddControllers().AddNewtonsoftJson();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Educational Materials API",
-        Description = "An ASP.NET Core Web API for managing Educational Materials"
-    }
-    );
-
-    options.EnableAnnotations();
-});
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false; // enable in production
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
-builder.Services.AddSingleton<IJwtAuthenticator>(new JwtAuthenticator(builder.Configuration["Jwt:Key"]));
-
-builder.Services.AddDbContext<EduMatApiDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["ConnectionStrings:EduMatApiDbConnection"]));
-builder.Services.AddDbContext<UserDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["ConnectionStrings:UserDbConnection"]));
-builder.Services.AddCors(options =>
-    options.AddPolicy(name: "AllowAllOrigins",
-                      builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
-                      )
-    );
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddScoped<IRepository<Author>, Repository<Author>>();
-builder.Services.AddScoped<IRepository<Material>, Repository<Material>>();
-builder.Services.AddScoped<IRepository<MaterialType>, Repository<MaterialType>>();
-builder.Services.AddScoped<IRepository<Review>, Repository<Review>>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddMvc(options => options.Filters.Add<EduMatApiExceptionFilter>());
-builder.Services.AddScoped<ILogger<EduMatApiExceptionFilter>, Logger<EduMatApiExceptionFilter>>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
