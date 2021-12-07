c8bc1b8728832c465b8f1dcc4b0d92350fa6ca6f
pikuch <55052790+pikuch@users.noreply.github.com>
2021-12-07 18:24:10 +0100

    Add admin register endpoint

--- EduMatApi/Controllers/UsersController.cs
+++ EduMatApi/Controllers/UsersController.cs
@@ -35,9 +35,9 @@ namespace EduMatApi.Controllers
         /// <summary>
         /// Authenticates user
         /// </summary>
-        /// <param name="userLoginDto">login and password</param>
+        /// <param name="userLoginDto">Login and password</param>
         [HttpPost("Login")]
-        [SwaggerOperation("Authenticates the user", "POST /User/Login")]
+        [SwaggerOperation("Authenticates the user", "POST /Users/Login")]
         [SwaggerResponse(StatusCodes.Status200OK, "User authorized")]
         [SwaggerResponse(StatusCodes.Status400BadRequest, "User data is invalid")]
         [SwaggerResponse(StatusCodes.Status401Unauthorized, "User not authorized")]
@@ -51,6 +51,51 @@ namespace EduMatApi.Controllers
             }
             _logger.LogInformation($"[{DateTime.Now}] Successful login by {userLoginDto.Login}");
             return Ok(token);
+        }
+
+        /// <summary>
+        /// Registers a new admin
+        /// </summary>
+        /// <param name="userRegisterDto">Login, email and password of the new admin</param>
+        [HttpPost("Register")]
+        [Authorize(Roles = "Admin")]
+        [SwaggerOperation("Registers a new admin", "POST /Users/Register")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Admin registered")]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "User data is invalid")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, "User not authorized")]
+        public async Task<IActionResult> Registration([FromBody] UserRegisterDto userRegisterDto)
+        {
+            // login already taken
+            var existingUser = await _userRepository.GetByLoginAsync(userRegisterDto.Login);
+            if (existingUser != null)
+            {
+                _logger.LogInformation($"[{DateTime.Now}] Failed to register new user because {userRegisterDto.Login} already exists.");
+                return BadRequest();
+            }
+
+            // email already taken
+            var allUsers = await _userRepository.GetAllAsync();
+            if (allUsers.Any(u => u.Email == userRegisterDto.Email))
+            {
+                _logger.LogInformation($"[{DateTime.Now}] Failed to register new user because user with email {userRegisterDto.Email} already exists.");
+                return BadRequest();
+            }
+
+            var newUser = _mapper.Map<User>(userRegisterDto);
+            newUser.Role = "Admin";
+
+            var addedUser = await _userRepository.AddAsync(newUser);
+
+            if (addedUser != null)
+            {
+                _logger.LogInformation($"[{DateTime.Now}] Successfully registered new user {addedUser.Login}.");
+                return Ok();
+            }
+            else
+            {
+                _logger.LogInformation($"[{DateTime.Now}] Failed to register new user for unknown reason.");
+                return BadRequest();
+            }
         }
     }
 }
--- EduMatApi/Models/Authentification/UserRegisterDto.cs
+++ EduMatApi/Models/Authentification/UserRegisterDto.cs
@@ -2,9 +2,8 @@
 
 namespace EduMatApi.Models.Authentification
 {
-    public class User
+    public class UserRegisterDto
     {
-        [Key]
         [Required]
         [StringLength(20, MinimumLength = 1)]
         public string Login { get; set; } = null!;
@@ -14,9 +13,7 @@ namespace EduMatApi.Models.Authentification
         public string Email { get; set; } = null!;
 
         [Required]
-        [StringLength(100, MinimumLength = 1)]  // Too short for production, too long for testing
+        [StringLength(100, MinimumLength = 1)]
         public string Password { get; set; } = null!;
-
-        public string Role { get; set; } = null!;
     }
 }
